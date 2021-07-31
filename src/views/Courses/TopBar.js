011072e02dbb0a4236a6dbaf6707a787e125285a
smida.montasser <Scoop007#>
2021-07-31 21:08:17 +0200

    add topbar background

--- src/views/Courses/TopBar.js
+++ src/views/Courses/TopBar.js
@@ -2,7 +2,7 @@ function TopBar(props) {
   return (
     <div className='topbar' id='top'>
       <div className='container'>
-        <h1 className='mb-4'>{props.title}</h1>
+        <h1 className='mb-4'>Virtual Student <br/>Orientation 2021/22</h1>
         <span className='category'>
           <i className={props.icon}></i>
           <span className='ms-2'>{props.category}</span>
