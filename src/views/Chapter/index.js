4b156a86a376ee902b1028052f3c7170185e87b8
smida.montasser <Scoop007#>
2021-08-04 03:14:32 +0200

    fix parse html on description

--- src/views/Chapter/index.js
+++ src/views/Chapter/index.js
@@ -305,7 +305,7 @@ function Chapter(props) {
           {sections.map((e, k) => (
             <React.Fragment key={k}>
               <h2>{parse(e.title)}</h2>
-              <p>{parse(e.description)}</p>
+              <p dangerouslySetInnerHTML={{ __html: parse(e.description) }}></p>
             </React.Fragment>
           ))}
           <Attachements />
