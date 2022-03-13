9c161789263bc8b3d5a8e634931da4b65e046076
Ahmed Meftah <contact@ahmedmeftah.com>
2022-03-13 18:46:33 +0100

    Create sidebar header

--- src/views/Chapter/SideBar/index.js
+++ src/views/Chapter/SideBar/index.js
@@ -1,11 +1,10 @@
 import { Redirect } from 'react-router-dom'
-import parse from 'html-react-parser'
-
 import { useEffect, useRef } from 'react'
 import SideBarItem from './SideBarItem'
 import Toogler from './Toogler'
 import Actions from './Actions'
 import Progress from './Progress'
+import SidebarHeader from './SidebarHeader'
 
 function SideBar(props) {
   const progress = ((parseInt(props.progress) ?? 0) / 100).toFixed(2)
@@ -53,24 +52,7 @@ function SideBar(props) {
       <Actions nextUrl={props.nextUrl} percent={percent} />
       <Progress percent={percent} />
       <div>
-        <div className='header'>
-          <div>
-            <span>{parse(props.program?.title ?? '')} Chapter Content</span>
-            <button className='btn btn-link close-side-bar' onClick={props.toogler}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='16'
-                height='16'
-                fill='currentColor'
-                viewBox='0 0 16 16'>
-                <path
-                  fillRule='evenodd'
-                  d='M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z'
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
+        <SidebarHeader toogler={props.toogler} title={props.program?.title ?? ''} />
         <ul className='lessons-list'>
           {sidebarList.map((e, k) => (
             <SideBarItem key={k} {...e} />
