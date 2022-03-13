9039b8764ebba715fc2876badbebf024df8ee6e5
Ahmed Meftah <contact@ahmedmeftah.com>
2022-03-13 18:41:25 +0100

    Move sidebar items to another file

--- src/views/Chapter/SideBar/index.js
+++ src/views/Chapter/SideBar/index.js
@@ -4,53 +4,7 @@ import parse from 'html-react-parser'
 import { ReactComponent as ChapterIcon } from '../../assets/svg/chapter.svg'
 import { ReactComponent as ListIcon } from '../../assets/svg/bullet-list-marked.svg'
 import { useEffect, useRef } from 'react'
-
-const formatTime = seconds => {
-  const m = parseInt(seconds / 60)
-  const s = parseInt(seconds % 60)
-  return `${m} min ${s} s`
-}
-
-function SideBarItem(props) {
-  const InnerLi = () => {
-    if (props.link)
-      return (
-        <div>
-          <Link to={`/program/${props.programId}/${props.id}`} disabled={true}>
-            <div className='title'>{parse(props.title)}</div>
-            <div className='time'>
-              <i className='fas me-2 fa-stopwatch' />
-              {formatTime(props.time)}
-            </div>
-          </Link>
-        </div>
-      )
-    return (
-      <div>
-        <div className='disabled'>
-          <div className='title'>{parse(props.title)}</div>
-          <div className='time'>
-            <i className='fas me-2 fa-stopwatch' />
-            {formatTime(props.time)}
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  const liClass = props.currentChapter ? 'active' : ''
-  return (
-    <li className={liClass}>
-      <div className='li-container'>
-        <label className='checkbox-container'>
-          <input type='checkbox' checked={props.checked} onChange={() => null} />
-          <span className='checkmark'></span>
-        </label>
-        <InnerLi />
-      </div>
-    </li>
-  )
-}
+import SideBarItem from './SideBarItem'
 
 function SideBar(props) {
   const progress = ((parseInt(props.progress) ?? 0) / 100).toFixed(2)
