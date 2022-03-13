482619da3f5892305e8d013c216d6ead8ed92476
Ahmed Meftah <contact@ahmedmeftah.com>
2022-03-13 18:37:48 +0100

    Create layout component

---
+++ src/views/Chapter/Layout.js
@@ -0,0 +1,37 @@
+import { Children, cloneElement, useEffect, useState } from 'react'
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+
+function Layout({ id, children }) {
+  const [nextUrl, setNextUrl] = useState('')
+
+  const { data: list = [] } = useQuery('get-programs-list', () =>
+    axios.get('/api/programs').then(res => res.data.map(e => e.nid))
+  )
+  const nextProgram = list[list.indexOf(id) + 1]
+
+  useEffect(() => {
+    axios
+      .get(`/api/program/${nextProgram}/chapters`)
+      .then(res => {
+        setNextUrl(`/program/${nextProgram}/${res.data?.[0]?.nid}`)
+      })
+      .catch(() => null)
+  }, [nextProgram])
+
+  return (
+    <>
+      <Header />
+      {Children.map(children, child => {
+        return cloneElement(child, {
+          nextUrl,
+        })
+      })}
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
